94335e1b75e950f50d5ff15232e2758d0a219902
Kelly Powers <kpowers.nj@gmail.com>
2019-12-29 18:23:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
     counter = 0 
     new_array = []
     while array[counter] do 
-      new_array<<array[counter]**
+      new_array<<(array[counter])**
       counter+=1 
-      puts new_array
     end
   p new_array
 end
